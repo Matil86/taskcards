cd18e2b99671651f64292a91f2f961f371635d16
Martin Hipp <hipp@sipgate.de>
2025-12-05 09:39:22 +0100

    💚 shouuld fix the CI Build

--- app/src/test/java/de/hipp/app/taskcards/ui/viewmodel/ListViewModelTest.kt
+++ app/src/test/java/de/hipp/app/taskcards/ui/viewmodel/ListViewModelTest.kt
@@ -22,6 +22,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
